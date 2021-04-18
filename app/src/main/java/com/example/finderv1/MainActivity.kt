742dc2e9b7bee1aa63d6270f17419fc8abee7566
Willer <hugues.willer@gmail.com>
2021-04-18 12:54:42 +0200

    LAyout

--- app/src/main/java/com/example/finderv1/MainActivity.kt
+++ app/src/main/java/com/example/finderv1/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.finderv1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finderv1.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //injecter le fragment dans notre boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
 
-        //faefsdadadaaf
-        //lol
-        //alisée,à poil sur skype hgfyff
-        //bonsoir
-        //Trop BIEN
-        //vraiment trop beau
 
     }
 }
