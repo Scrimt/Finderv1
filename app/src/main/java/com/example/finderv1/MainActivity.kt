5b97d4aa4e86a5e48f4f7c0045ad0f2d7b88c903
Willer <hugues.willer@gmail.com>
2021-05-05 11:51:52 +0200

    LAyout

--- app/src/main/java/com/example/finderv1/MainActivity.kt
+++ app/src/main/java/com/example/finderv1/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.finderv1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import com.example.finderv1.fragments.AccountFragment
 import com.example.finderv1.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity()  {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,17 +22,20 @@ class MainActivity : AppCompatActivity()  {
         transaction.addToBackStack(null)
         transaction.commit()
 
+        val button = findViewById<Button>(R.id.button)
+            button.setOnClickListener {
+                val intent = Intent(this,AccountFragment::class.java)
+                startActivity(intent)
+            }
+
+
         // là je vais créer un spinner l'équipe
 
-        Spinner spinner1 = findViewById(R.id.spinnerGender);
+        /*Spinner spinner1 = findViewById(R.id.spinnerGender);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context: this, R.array.genre, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
-
-
-
+        spinner.setOnItemSelectedListener(this);*/
 
     }
 
